8df5db0a33b23c7603e2a77e8cb3de9aab94e549
Youssef <youssefyaser2020@gmail.com>
2025-06-01 15:22:56 +0300

    add initial App component with routing and layout

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import About from "./Pages/About";
+import "./styles/App.css";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import Menu from "./Pages/Menu";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="menu" element={<Menu />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
